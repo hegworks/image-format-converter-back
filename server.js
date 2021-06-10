1a6410380a1ff2e3a3fcab876f997f7985efae0a
Pourya Pourbagheri <34511048+pouryapb@users.noreply.github.com>
2021-06-10 17:21:17 +0430

    better start script cors handle convert url encoded data to json convert to ES6

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-const express = require("express");
-const multer = require("multer");
-const gm = require("gm");
+import express, { urlencoded, json } from "express";
+import multer, { diskStorage } from "multer";
+import gm from "gm";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
-const storage = multer.diskStorage({
+const storage = diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads");
   },
   filename: function (req, file, cb) {
-    cb(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
+    cb(
+      null,
+      new Date().toISOString().replace(/:/g, "-") +
+        Math.random() +
+        file.originalname.match(/(.*)(\.[0-9a-z]+$)/)[2]
+    );
   },
 });
 
 const fileFilter = (req, file, cb) => {
   const type = file.mimetype.split("/")[0];
-  if (type === "image" || type === "video") {
+  if (type === "image") {
     cb(null, true);
   } else {
     cb(null, false);
@@ -29,17 +38,39 @@ const upload = multer({
   fileFilter: fileFilter,
 });
 
-app.post("/convert", upload.single("postmedia"), (req, res) => {
-  const input = req.file;
-  const outputpath = "./" + input.path + "" + ".bmp";
-  gm("./" + input.path).write(outputpath, function (err) {
+// CORS handling
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested_With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Methods", "POST");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// Convert url encoded data to json
+app.use(urlencoded({ extended: false }));
+app.use(json());
+
+app.post("/convert", upload.single("image"), (req, res) => {
+  const file = req.file;
+  const format = req.body.format;
+  const inputPath = file.path;
+  const outputPath = `${file.destination}/${
+    file.filename.match(/(.*)(\.[0-9a-z]+$)/)[1]
+  }.${format}`;
+
+  gm(inputPath).write(outputPath, (err, stdout, stderr, cmd) => {
     if (err) console.log(err);
     else {
-      //   console.log("done");
-      res.sendFile(__dirname + outputpath.slice(1));
+      console.log(cmd);
+      res.sendFile(__dirname + outputPath.slice(1));
     }
   });
-  // res.send("Hello World");
 });
 
 app.listen(process.env.PORT || 2999);
